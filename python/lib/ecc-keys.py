80a94dada6bb7cd8aff507a736a1a6e01250c8c9
Steve Johnson <s.johnson@cablelabs.com>
2018-03-02 13:41:02 -0700

    missing lib

---
+++ python/lib/ecc-keys.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+from OpenSSL import SSL
+
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.backends.openssl.backend import backend
+from cryptography.hazmat.primitives import serialization
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+import os
+
+# Generate Private Key
+def generateKey(keyName, path=None):
+	private_key = ec.generate_private_key(ec.SECP384R1(), backend)
+
+	# Save as PEM files
+	if path is not None:
+		with open(path+'/'+keyName, "wb") as f:
+		    f.write(privateKeyPEM(private_key))
+		
+		with open(path+'/'+keyName+".pub", "wb") as f:
+		    f.write(publicKeyPEM(private_key.public_key()))
+	
+	return private_key
+
+def privateKeyPEM(private_key):
+	return private_key.private_bytes(
+		encoding=serialization.Encoding.PEM,
+		format=serialization.PrivateFormat.TraditionalOpenSSL,
+		encryption_algorithm=serialization.NoEncryption())
+
+def publicKeyPEM(public_key):
+	return public_key.public_bytes(
+		encoding=serialization.Encoding.PEM,
+		format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+def generateCSR(key, csrName, path=None):
+	csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+	    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+	    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"CO"),
+	    x509.NameAttribute(NameOID.LOCALITY_NAME, u"Louisville"),
+	    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"CableLabs"),
+	    x509.NameAttribute(NameOID.COMMON_NAME, u"medicalDevice"),
+	])
+	# Sign the CSR with our private key.
+	).sign(key, hashes.SHA256(), backend)
+
+	if path is not None:
+		# Write our CSR out to disk.
+		with open(path+'/'+csrName+".pem", "wb") as f:
+		    f.write(csr.public_bytes(serialization.Encoding.PEM))
+	return csr
+
+def loadPrivateKey(name, path):
+	with open(path+'/'+name, "rb") as key_file:
+	    key = serialization.load_pem_private_key(
+	        key_file.read(),
+	        password=None,
+	        backend=backend
+	    )
+	    return key
+
+def loadPublicKey(name, path):
+	with open(path+'/'+name+'.pub', "rb") as key_file:
+	    key = serialization.load_pem_public_key(
+	        key_file.read(),
+	        backend=backend
+	    )
+	    return key
+
+if __name__ == '__main__':
+
+	keyPath = '../ssh'
+	if not os.path.exists(keyPath):
+	    os.makedirs(keyPath)
+
+	# Generate key pair
+	private_key = generateKey("wifiKey", keyPath)
+	public_key = private_key.public_key()
+
+	# Generate a CSR
+	csr = generateCSR(private_key, "wifiCSR", keyPath)
+
+	# Read keys back in from PEM files
+	testKey = loadPrivateKey("wifiKey", keyPath)
+	testKeyPub = loadPublicKey("wifiKey", keyPath)
---
+++ python/lib/wpa-supplicant.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# This module is used to configure wpa_supplicant. 
+
+import os
+import glob
+import sys
+import subprocess
+
+__all__ = ["wpa_reset", "wpa_add_subscriber"]
+
+# exposed methods
+def wpa_reset():
+	# remove our credentials and configuration
+	rm_silent('/etc/micronets/networks/subscriber/network.config')
+	rm_silent('/etc/micronets/networks/subscriber/ca.pem')
+	rm_silent('/etc/micronets/networks/subscriber/wifi_key')
+	rm_silent('/etc/micronets/networks/subscriber/wifi.crt')
+
+	# create initial wpa_supplicant.conf
+	f = open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w')
+	f.write("ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n")
+	f.write("update_config=1\n")
+	f.write('\n')
+
+	f.close();
+
+	# add default network
+	add_network('default', 0)
+
+
+def wpa_add_subscriber(ssid, ca_cert, wifi_cert, wifi_key, identity):
+
+	# Start with default configuration
+	wpa_reset()
+
+	# Generate subscriber's network configuration file
+	with open('/etc/micronets/networks/subscriber/network.config', 'w') as f:
+		f.write('ssid="' + ssid + '"\n')
+		f.write('scan_ssid=1\n')
+		f.write('key_mgmt=WPA-EAP\n')
+		f.write('group=CCMP TKIP\n')
+		f.write('eap=TLS\n')
+		f.write('identity="' + identity + '"\n')
+		f.write('ca_cert="/etc/micronets/networks/subscriber/ca.pem"\n')
+		f.write('client_cert="/etc/micronets/networks/subscriber/wifi.crt"\n')
+		f.write('private_key="/etc/micronets/networks/subscriber/wifi_key"\n')
+
+		f.close()
+
+	# Save keys and certs
+	with open('/etc/micronets/networks/subscriber/ca.pem', 'wb') as f:
+	    f.write(ca_cert)
+	with open('/etc/micronets/networks/subscriber/wifi.crt', 'wb') as f:
+	    f.write(wifi_cert)
+	with open('/etc/micronets/networks/subscriber/wifi_key', 'wb') as f:
+	    f.write(wifi_key)
+
+	# add subscriber network
+	add_network('subscriber', 1)
+
+
+def rm_silent(filename):
+	try:
+	    os.remove(filename)
+	except OSError:
+		print 'failed to remove file: {}'.format(filename)
+		pass
+
+def add_network(network, priority):
+
+	infile = '/etc/micronets/networks/'+network+'/network.config'
+	outfile = '/etc/wpa_supplicant/wpa_supplicant.conf'
+
+	with open(outfile, 'a') as fout, open(infile, 'r') as fin:
+		fout.write('')
+		fout.write('network={\n')
+		fout.write('    priority={}\n'.format(priority))
+
+		for line in fin:
+			fout.write('    {}'.format(line))
+
+		fout.write('}\n')
+
+if __name__ == '__main__':
+
+	if len(sys.argv) > 1 and sys.argv[1] == 'reset':
+		print "reset"
+		wpa_reset()
+	else:
+		print "add"
+		wpa_add_subscriber("Grandma's WiFi", "ca certificate", "wifi certificate", "wifi key", "micronets")
+
+	print "\n"
+	print "wpa_supplicant.conf:\n"
+	with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'r') as fin:
+		#for line in fin:
+		#	sys.stdout.write(line)
+		print fin.read()
+
+	print "/networks/subscriber:\n"
+	print os.listdir('/etc/micronets/networks/subscriber')
+
+
+
+
+
+
+
